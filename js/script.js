420a070c8f0d1b69e02da9580c6873380f355033
Emma Janani <99275087+EmmaJanani13@users.noreply.github.com>
2022-04-27 15:00:02 +0000

    Finished Webpage

--- js/script.js
+++ js/script.js
@@ -9,18 +9,22 @@ function calcFuel() {
   let tripDistance = parseFloat(document.getElementById('distance').value)
   let fuelEfficiency = parseFloat(document.getElementById('efficiency').value)
   let fuelPrice = parseFloat(document.getElementById('price').value)
-
-  // if tripDistance is more than 200km, display "Bon Voyage and Safe Travels!"
-if (tripDistance > 200) {
-  greeting = "Bon Voyage and Safe Travels!"
-}
   
   // calculate the amount of fuel needed and fuel cost for a car trip
-  let fuel_required = tripDistance / fuelEfficiency
+  let fuel_required = tripDistance * (fuelEfficiency / 100)
   let fuel_cost = tripDistance * (fuelEfficiency / 100) * fuelPrice
 
-  // display the results
-  document.getElementById('fuel-required').innerHTML = fuel_required.toFixed(2) + " litres of fuel are required for the trip,"
-  document.getElementById('fuel-cost').innerHTML = " so the fuel cost is $" + fuel_cost.toFixed(2)
-  document.getElementbyId('greeting').innerHTML = greeting 
+  // if tripDistance is more than 200km, display "Bon Voyage and Safe Travels!"
+  if (tripDistance > 200) {
+    let greeting = "Bon Voyage and Safe Travels!"
+    document.getElementById('greeting').innerHTML = "" + greeting
+    document.getElementById('fuel-required').innerHTML = fuel_required.toFixed(2) + " litres of fuel are required for the trip,"
+    document.getElementById('fuel-cost').innerHTML = " so the fuel cost is $" + fuel_cost.toFixed(2)
+  }
+
+  else {
+    // display the results
+    document.getElementById('fuel-required').innerHTML = fuel_required.toFixed(2) + " litres of fuel are required for the trip,"
+    document.getElementById('fuel-cost').innerHTML = " so the fuel cost is $" + fuel_cost.toFixed(2)
+  }
 }
