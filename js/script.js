e0c75c914cccd58b22f6e54878b006f77c746df1
Emma Janani <99275087+EmmaJanani13@users.noreply.github.com>
2022-04-27 04:02:01 +0000

    Trying to do If Statement

--- js/script.js
+++ js/script.js
@@ -10,6 +10,11 @@ function calcFuel() {
   let fuelEfficiency = parseFloat(document.getElementById('efficiency').value)
   let fuelPrice = parseFloat(document.getElementById('price').value)
 
+  // if tripDistance is more than 200km, display "Bon Voyage and Safe Travels!"
+if (tripDistance > 200) {
+  greeting = "Bon Voyage and Safe Travels!"
+}
+  
   // calculate the amount of fuel needed and fuel cost for a car trip
   let fuel_required = tripDistance / fuelEfficiency
   let fuel_cost = tripDistance * (fuelEfficiency / 100) * fuelPrice
@@ -17,4 +22,5 @@ function calcFuel() {
   // display the results
   document.getElementById('fuel-required').innerHTML = fuel_required.toFixed(2) + " litres of fuel are required for the trip,"
   document.getElementById('fuel-cost').innerHTML = " so the fuel cost is $" + fuel_cost.toFixed(2)
+  document.getElementbyId('greeting').innerHTML = greeting 
 }
